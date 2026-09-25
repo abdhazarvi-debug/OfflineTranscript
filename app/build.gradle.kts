b5a0c5564ccc83192bf0ff895bdc6a987cf9b236
abdhazarvi-debug <abdhazarvi@gmail.com>
2026-09-25 13:40:22 +0500

    Bump app version to 3.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.offlinetranscript"
         minSdk = 24
         targetSdk = 35
-        versionCode = 3
-        versionName = "3.0"
+        versionCode = 4
+        versionName = "3.2"
         ndk {
             abiFilters += "arm64-v8a"
         }
